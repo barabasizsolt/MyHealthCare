79b5e5a9c030b1dc2845c65c0b2217f8432bb854
Barabási Róbert - Zsolt <barabasi.robert.zsolt@gmail.com>
2021-11-07 14:42:59 +0200

    feat: forgot password merged into main

--- src/client/app/src/main/java/com/example/myhealthcareapp/MainActivity.kt
+++ src/client/app/src/main/java/com/example/myhealthcareapp/MainActivity.kt
@@ -3,40 +3,18 @@ package com.example.myhealthcareapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-<<<<<<<<< Temporary merge branch 1
-import com.example.myhealthcareapp.register.RegisterFragment
-=========
 import com.example.myhealthcareapp.login.LoginFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import com.example.myhealthcareapp.register.RegisterFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-<<<<<<<<< Temporary merge branch 1
-        replaceFragment(RegisterFragment(), R.id.activity_fragment_container)
-=========
-
         replaceFragment(LoginFragment(), R.id.fragment_container)
->>>>>>>>> Temporary merge branch 2
     }
 
     fun replaceFragment(fragment: Fragment, containerId: Int, addToBackStack:Boolean = false, withAnimation:Boolean = false){
         val transaction = supportFragmentManager.beginTransaction()
-<<<<<<<<< Temporary merge branch 1
-//        when(withAnimation){
-//            true -> {
-//                transaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out)
-//            }
-//        }
-=========
-/*        when(withAnimation){
-            true -> {
-                transaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out)
-            }
-        }*/
->>>>>>>>> Temporary merge branch 2
         transaction.replace(containerId, fragment)
         when(addToBackStack){
             true -> {
@@ -44,8 +22,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
         transaction.commit()
-<<<<<<<<< Temporary merge branch 1
-=========
     }
 
     private fun initBottomNavigation(){
@@ -66,6 +42,5 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
->>>>>>>>> Temporary merge branch 2
     }
 }
