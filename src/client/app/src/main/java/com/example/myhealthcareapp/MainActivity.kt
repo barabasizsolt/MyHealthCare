96c852d0ea9343c7d9a0203dba9bbcf4372e5d1d
donathdaniel <57301890+donathdaniel@users.noreply.github.com>
2021-11-07 14:04:45 +0200

    Login part design

--- src/client/app/src/main/java/com/example/myhealthcareapp/MainActivity.kt
+++ src/client/app/src/main/java/com/example/myhealthcareapp/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.myhealthcareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myhealthcareapp.login.LoginFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        replaceFragment(LoginFragment(), R.id.fragment_container)
+    }
+
+    fun replaceFragment(fragment: Fragment, containerId: Int, addToBackStack:Boolean = false, withAnimation:Boolean = false){
+        val transaction = supportFragmentManager.beginTransaction()
+/*        when(withAnimation){
+            true -> {
+                transaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out)
+            }
+        }*/
+        transaction.replace(containerId, fragment)
+        when(addToBackStack){
+            true -> {
+                transaction.addToBackStack(null)
+            }
+        }
+        transaction.commit()
+    }
+
+    private fun initBottomNavigation(){
+        bottomNavigation.setOnItemSelectedListener {item ->
+            when(item.itemId) {
+                R.id.make_appointment -> {
+                    //TODO: implement it
+                    true
+                }
+                R.id.feedback -> {
+                    //TODO: implement it
+                    true
+                }
+                R.id.my_appointments -> {
+                    //TODO: implement it
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
