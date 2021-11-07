206289e1fc9b0c26c76f8f5304f1c93beead70dc
Barabási Róbert - Zsolt <barabasi.robert.zsolt@gmail.com>
2021-11-07 14:03:52 +0200

    feat: register fragment added

--- src/client/app/src/main/java/com/example/myhealthcareapp/MainActivity.kt
+++ src/client/app/src/main/java/com/example/myhealthcareapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.myhealthcareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myhealthcareapp.register.RegisterFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(RegisterFragment(), R.id.activity_fragment_container)
+    }
+
+    fun replaceFragment(fragment: Fragment, containerId: Int, addToBackStack:Boolean = false, withAnimation:Boolean = false){
+        val transaction = supportFragmentManager.beginTransaction()
+//        when(withAnimation){
+//            true -> {
+//                transaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out)
+//            }
+//        }
+        transaction.replace(containerId, fragment)
+        when(addToBackStack){
+            true -> {
+                transaction.addToBackStack(null)
+            }
+        }
+        transaction.commit()
     }
 }
