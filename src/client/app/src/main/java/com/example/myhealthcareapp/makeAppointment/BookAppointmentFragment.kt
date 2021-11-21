ef9d851762dc9230d7a0e528e17afee1af7496fb
Barabási Róbert - Zsolt <barabasi.robert.zsolt@gmail.com>
2021-11-21 15:14:40 +0200

    feat: datePicker with custom available dates added

--- src/client/app/src/main/java/com/example/myhealthcareapp/makeAppointment/BookAppointmentFragment.kt
+++ src/client/app/src/main/java/com/example/myhealthcareapp/makeAppointment/BookAppointmentFragment.kt
@@ -1,7 +1,6 @@
 package com.example.myhealthcareapp.makeAppointment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,18 +10,26 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myhealthcareapp.R
 import com.example.myhealthcareapp.adapters.BookAppointmentAdapter
-import com.example.myhealthcareapp.constants.Constant.getDate
 import com.example.myhealthcareapp.models.Medic
-import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import android.util.Log
+import com.example.myhealthcareapp.models.CustomDate
+import java.text.SimpleDateFormat
+import java.util.*
 
-class BookAppointmentFragment : Fragment() {
+class BookAppointmentFragment : Fragment(),
+    DatePickerDialog.OnDateSetListener
+{
     private lateinit var recyclerview: RecyclerView
     private lateinit var dateButton: FloatingActionButton
     private lateinit var timeButton: FloatingActionButton
     private lateinit var bookAppointment: Button
     private lateinit var appointmentDate: TextView
     private lateinit var appointmentTime: TextView
+    private lateinit var calendar: Calendar
+    private lateinit var formatter: SimpleDateFormat
+    private lateinit var availableDays: MutableList<CustomDate>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,14 +45,41 @@ class BookAppointmentFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         dateButton.setOnClickListener {
-            val datePicker = MaterialDatePicker.Builder.datePicker()
-                    .setTitleText("Select date")
-                    .build()
-            datePicker.show(childFragmentManager, "tag")
-            datePicker.addOnPositiveButtonClickListener {
-                val date = getDate(it)
-                appointmentDate.text = date
+            val year = calendar.get(Calendar.YEAR) ;
+            val month = calendar.get(Calendar.MONTH);
+            val day = calendar.get(Calendar.DAY_OF_MONTH);
+            val hour = calendar.get(Calendar.HOUR_OF_DAY);
+            val minute = calendar.get(Calendar.MINUTE);
+
+            val datePickerDialog = DatePickerDialog.newInstance(this, year, month, day)
+            datePickerDialog.isThemeDark = true
+            datePickerDialog.showYearPickerFirst(false)
+            datePickerDialog.setTitle("Set appointment date")
+
+            // Setting Min Date to today date
+            val minDate = Calendar.getInstance()
+            datePickerDialog.minDate = minDate
+
+            // Setting Max Date to next 1 years
+            val maxDate = Calendar.getInstance()
+            maxDate[Calendar.YEAR] = year + 1
+            datePickerDialog.maxDate = maxDate
+
+            //Disable all SUNDAYS, SATURDAYS and occupied dates between Min and Max Dates
+            var loopDate: Calendar = minDate
+            while (minDate.before(maxDate)) {
+                val dayOfWeek = loopDate[Calendar.DAY_OF_WEEK]
+                val customDate = CustomDate(loopDate[Calendar.YEAR], loopDate[Calendar.MONTH], loopDate[Calendar.DATE])
+                if (dayOfWeek == Calendar.SUNDAY || dayOfWeek == Calendar.SATURDAY || !availableDays.contains(customDate)) {
+                    val disabledDays = arrayOfNulls<Calendar>(1)
+                    disabledDays[0] = loopDate
+                    datePickerDialog.disabledDays = disabledDays
+                }
+                minDate.add(Calendar.DATE, 1)
+                loopDate = minDate
             }
+
+            datePickerDialog.show(childFragmentManager, "DatePickerDialog")
         }
         timeButton.setOnClickListener {
         }
@@ -55,6 +89,8 @@ class BookAppointmentFragment : Fragment() {
     }
 
     private fun setupUI(view: View){
+        calendar = Calendar.getInstance()
+        formatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         recyclerview = view.findViewById(R.id.medic_recyclerview)
         recyclerview.adapter = BookAppointmentAdapter(generateDummyList())
         recyclerview.setHasFixedSize(true)
@@ -63,6 +99,23 @@ class BookAppointmentFragment : Fragment() {
         bookAppointment = view.findViewById(R.id.make_appointment_btn)
         appointmentDate = view.findViewById(R.id.appointment_date)
         appointmentTime = view.findViewById(R.id.appointment_time)
+        availableDays = mutableListOf()
+
+        //Dummy Data for available days
+        for(i in 10 until 20) {
+            val date = "2021-12-$i"
+            formatter.parse(date)
+            val cal = formatter.calendar
+            val customDate = CustomDate(cal[Calendar.YEAR], cal[Calendar.MONTH], cal[Calendar.DATE])
+            availableDays.add(customDate)
+        }
+        for(i in 2 until 25) {
+            val date = "2022-01-$i"
+            formatter.parse(date)
+            val cal = formatter.calendar
+            val customDate = CustomDate(cal[Calendar.YEAR], cal[Calendar.MONTH], cal[Calendar.DATE])
+            availableDays.add(customDate)
+        }
     }
 
     private fun generateDummyList(): MutableList<Medic> {
@@ -72,5 +125,9 @@ class BookAppointmentFragment : Fragment() {
             list += item
         }
         return list
+    }
+
+    override fun onDateSet(view: DatePickerDialog?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+        Log.d("DATE", "$year $monthOfYear $dayOfMonth")
     }
 }
