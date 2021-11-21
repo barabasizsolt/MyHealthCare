01a9fcf4c7f1c9ef31bb67c7a210f8fb62860f82
Barabási Róbert - Zsolt <barabasi.robert.zsolt@gmail.com>
2021-11-21 18:42:50 +0200

    feat: BookAppointment Fragment added

--- src/client/app/src/main/java/com/example/myhealthcareapp/adapters/BookAppointmentAdapter.kt
+++ src/client/app/src/main/java/com/example/myhealthcareapp/adapters/BookAppointmentAdapter.kt
@@ -1,15 +1,22 @@
 package com.example.myhealthcareapp.adapters
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.myhealthcareapp.R
+import com.example.myhealthcareapp.interfaces.OnItemClickListener
 import com.example.myhealthcareapp.models.Medic
 
-class BookAppointmentAdapter(private val mList: List<Medic>) : RecyclerView.Adapter<BookAppointmentAdapter.ViewHolder>() {
+class BookAppointmentAdapter(private val mList: List<Medic>, private val listener : OnItemClickListener) : RecyclerView.Adapter<BookAppointmentAdapter.ViewHolder>() {
+    private var selectedPosition = 0;
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.medic_recyclerview_element, parent, false)
@@ -19,13 +26,45 @@ class BookAppointmentAdapter(private val mList: List<Medic>) : RecyclerView.Adap
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val itemsViewModel = mList[position]
         //holder.medicImage
+
+        Glide.with(holder.itemView.context)
+            .load(R.drawable.ic_avatar)
+            .placeholder(R.drawable.ic_avatar)
+            .error(R.drawable.ic_avatar)
+            .circleCrop()
+            .into(holder.medicImage)
+
         holder.medicName.text = itemsViewModel.name
+        if(position == selectedPosition){
+            holder.background.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.colorPrimary))
+        }
+        else{
+            holder.background.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.colorHintText))
+        }
     }
 
     override fun getItemCount() = mList.size
 
-    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+    inner class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView), View.OnClickListener {
         val medicImage: ImageView = itemView.findViewById(R.id.medic_image)
         val medicName: TextView = itemView.findViewById(R.id.medic_name)
+        val background: CardView = itemView.findViewById(R.id.medic_card_view)
+
+        init{
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = this.absoluteAdapterPosition
+            if( position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position)
+            }
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun selectMedic(position: Int){
+       selectedPosition = position
+        notifyDataSetChanged()
     }
 }
--- src/client/app/src/main/java/com/example/myhealthcareapp/makeAppointment/BookAppointmentFragment.kt
+++ src/client/app/src/main/java/com/example/myhealthcareapp/makeAppointment/BookAppointmentFragment.kt
@@ -14,22 +14,43 @@ import com.example.myhealthcareapp.models.Medic
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import android.util.Log
+import android.widget.Toast
+import androidx.core.view.get
+import com.example.myhealthcareapp.MainActivity
+import com.example.myhealthcareapp.interfaces.OnItemClickListener
 import com.example.myhealthcareapp.models.CustomDate
+import com.example.myhealthcareapp.models.Hospital
+import com.example.myhealthcareapp.models.MedicalDepartment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class BookAppointmentFragment : Fragment(),
-    DatePickerDialog.OnDateSetListener
+    DatePickerDialog.OnDateSetListener,
+    OnItemClickListener
 {
     private lateinit var recyclerview: RecyclerView
+    private lateinit var adapter: BookAppointmentAdapter
     private lateinit var dateButton: FloatingActionButton
     private lateinit var timeButton: FloatingActionButton
     private lateinit var bookAppointment: Button
     private lateinit var appointmentDate: TextView
+    private lateinit var appointmentDateLayout: TextInputLayout
     private lateinit var appointmentTime: TextView
+    private lateinit var appointmentTimeLayout: TextInputLayout
     private lateinit var calendar: Calendar
     private lateinit var formatter: SimpleDateFormat
     private lateinit var availableDays: MutableList<CustomDate>
+    private lateinit var availableHours: MutableList<String>
+
+    private lateinit var currentHospital: Hospital
+    private lateinit var currentDepartment: MedicalDepartment
+    private lateinit var currentMedic: Medic
+
+    //Dummy List for medics
+    private lateinit var medics: MutableList<Medic>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +58,8 @@ class BookAppointmentFragment : Fragment(),
     ): View? {
         val view = inflater.inflate(R.layout.fragment_book_appointment, container, false)
 
+        (activity as MainActivity).topAppBar.menu[0].isVisible = false
+        (activity as MainActivity).topAppBar.menu[1].isVisible = false
         setupUI(view)
 
         return view
@@ -45,11 +68,11 @@ class BookAppointmentFragment : Fragment(),
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         dateButton.setOnClickListener {
-            val year = calendar.get(Calendar.YEAR) ;
-            val month = calendar.get(Calendar.MONTH);
-            val day = calendar.get(Calendar.DAY_OF_MONTH);
-            val hour = calendar.get(Calendar.HOUR_OF_DAY);
-            val minute = calendar.get(Calendar.MINUTE);
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            val hour = calendar.get(Calendar.HOUR_OF_DAY)
+            val minute = calendar.get(Calendar.MINUTE)
 
             val datePickerDialog = DatePickerDialog.newInstance(this, year, month, day)
             datePickerDialog.isThemeDark = true
@@ -82,9 +105,38 @@ class BookAppointmentFragment : Fragment(),
             datePickerDialog.show(childFragmentManager, "DatePickerDialog")
         }
         timeButton.setOnClickListener {
+            var checkedItem = 1
+
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(resources.getString(R.string.select_time_interval))
+                .setNeutralButton(resources.getString(R.string.cancel)) { _, _ -> }
+                .setPositiveButton(resources.getString(R.string.ok)) { _, _ ->
+                    appointmentTime.text = availableHours[checkedItem]
+                }
+                .setSingleChoiceItems(availableHours.toTypedArray(), checkedItem) { _, which ->
+                    checkedItem = which
+                }
+                .show()
         }
         bookAppointment.setOnClickListener {
+            if(validateInput()){
+                val summary = arrayOf(
+                    "Hospital: " + "Policlinica 2", //Hospital name,
+                    "Department: " + "Neurology", //Department name,
+                    "Medic: " + currentMedic.name, //Medic name
+                    "Date & Time: " + appointmentDate.text + ", " + appointmentTime.text, //Appointment date & time
+                )
 
+                MaterialAlertDialogBuilder(requireContext())
+                    .setTitle(resources.getString(R.string.summary))
+                    .setItems(summary) {_, _ ->}
+                    .setNeutralButton(resources.getString(R.string.cancel)) { _, _ -> }
+                    .setPositiveButton(resources.getString(R.string.accept)) { _, _ ->
+                        Toast.makeText(requireContext(), "Appointment added", Toast.LENGTH_SHORT).show()
+                        //TODO navigate to myAppointmentsFragment
+                    }
+                    .show()
+            }
         }
     }
 
@@ -92,14 +144,20 @@ class BookAppointmentFragment : Fragment(),
         calendar = Calendar.getInstance()
         formatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         recyclerview = view.findViewById(R.id.medic_recyclerview)
-        recyclerview.adapter = BookAppointmentAdapter(generateDummyList())
+        medics = generateDummyList()
+        currentMedic = medics[0]
+        adapter = BookAppointmentAdapter(medics, this)
+        recyclerview.adapter = adapter
         recyclerview.setHasFixedSize(true)
         dateButton = view.findViewById(R.id.appointment_date_btn)
         timeButton = view.findViewById(R.id.appointment_time_btn)
         bookAppointment = view.findViewById(R.id.make_appointment_btn)
         appointmentDate = view.findViewById(R.id.appointment_date)
+        appointmentDateLayout = view.findViewById(R.id.appointment_date_layout)
         appointmentTime = view.findViewById(R.id.appointment_time)
+        appointmentTimeLayout = view.findViewById(R.id.appointment_time_layout)
         availableDays = mutableListOf()
+        availableHours = mutableListOf()
 
         //Dummy Data for available days
         for(i in 10 until 20) {
@@ -116,18 +174,60 @@ class BookAppointmentFragment : Fragment(),
             val customDate = CustomDate(cal[Calendar.YEAR], cal[Calendar.MONTH], cal[Calendar.DATE])
             availableDays.add(customDate)
         }
+
+        //Dummy data for available hours
+        for(i in 10 until 20){
+            val hour = "12:30 - 14:$i"
+            availableHours.add(hour)
+        }
     }
 
     private fun generateDummyList(): MutableList<Medic> {
         val list : MutableList<Medic> = mutableListOf()
         for (i in 0 until 20) {
-            val item = Medic(i, "Dr House")
+            val item = Medic(i, "Dr House$i")
             list += item
         }
         return list
     }
 
-    override fun onDateSet(view: DatePickerDialog?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+    override fun onDateSet(picker: DatePickerDialog?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
         Log.d("DATE", "$year $monthOfYear $dayOfMonth")
+        var month = (monthOfYear + 1).toString()
+        if(monthOfYear + 1 < 10){
+            month = "0${(monthOfYear + 1)}"
+        }
+        var day = (dayOfMonth + 1).toString()
+        if(dayOfMonth < 10){
+            day = "0$dayOfMonth"
+        }
+        val date = "$year-$month-$day"
+        appointmentDate.text = date
+    }
+
+    override fun onItemClick(position: Int) {
+        currentMedic = medics[position]
+        Log.d("Poz", currentMedic.toString())
+        adapter.selectMedic(position)
+        appointmentDate.text = null
+        appointmentTime.text = null
+       //TODO: call API with the newly selected medic
+    }
+
+    private fun validateInput(): Boolean {
+        appointmentDateLayout.error = null
+        appointmentTimeLayout.error = null
+
+        when{
+            appointmentDate.text.toString().isEmpty() -> {
+                appointmentDateLayout.error = getString(R.string.error)
+                return false
+            }
+            appointmentTime.text.toString().isEmpty() -> {
+                appointmentTimeLayout.error = getString(R.string.error)
+                return false
+            }
+        }
+        return true
     }
 }
