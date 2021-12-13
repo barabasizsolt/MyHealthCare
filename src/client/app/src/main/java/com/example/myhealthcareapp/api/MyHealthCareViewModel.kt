320167b13a6ea216700e5f5dc206a02d4bb0835b
Zsolt Barabási <zsolt.barabasi@halcyonmobile.com>
2021-12-13 22:08:55 +0200

    feat: hospitals received from api

---
+++ src/client/app/src/main/java/com/example/myhealthcareapp/api/MyHealthCareViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.myhealthcareapp.api
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myhealthcareapp.models.response.HospitalResponse
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class MyHealthCareViewModel(private val repository: MyHealthCareRepository) : ViewModel() {
+    val hospitals: MutableLiveData<Response<HospitalResponse>> = MutableLiveData()
+
+    fun loadHospitals() {
+        viewModelScope.launch {
+            val response = repository.getHospitals()
+            hospitals.value = response
+        }
+    }
+}
--- src/client/app/src/main/java/com/example/myhealthcareapp/models/Hospital.kt
+++ src/client/app/src/main/java/com/example/myhealthcareapp/models/Hospital.kt
@@ -1,11 +1,20 @@
 package com.example.myhealthcareapp.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Hospital (
+    @SerializedName("id")
     val hospitalId : Int,
+    @SerializedName("name")
     val hospitalName : String,
+    @SerializedName("contact")
     val hospitalPhoneNumber : String,
+    @SerializedName("address")
     val hospitalAddress : String,
+    @SerializedName("description")
     val description : String,
+    @SerializedName("longitude_coordinate")
     val longitudeCoordinate : String,
+    @SerializedName("latitude_coordinate")
     val latitudeCoordinate : String
 )
